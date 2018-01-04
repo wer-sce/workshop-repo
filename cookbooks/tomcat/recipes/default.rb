a0a59072278495d4427cd969b41f015c56d8a3fc
Werner Kraus <werner@scerus.com>
2018-01-04 22:00:18 +0100

    initial commit tomcat cookbook

---
+++ cookbooks/tomcat/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: tomcat
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ cookbooks/tomcat/recipes/server.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: tomcat
+# Recipe:: server
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
